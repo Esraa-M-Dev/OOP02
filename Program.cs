e09daafc5f81908d8409fbe27dd5528e4d3c240b
Esraa Mohamed <esomo60@gmail.com>
2026-09-04 16:42:49 +0300

    Theoretical Questions

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Theoretical Questions
+            //Question1
+            //a-struct:is a value type , the variables created from it is independent(holds data),modify by one of the doesn't affect the other after assignment(each has own place)
+            //class:is a reference type , the variables created from it is dependent(holds reference),modify by one of the affect the other after assignment(same reference=>same object)
+            //b-class is suitable for large applications than struct,class is long lived in memory which make it appropriate to complex data,class supports inheritance unlike struct
+            //
+            //Question2
+            //a-Parent:Shipment
+            //b-Child:ExpressShipment
+            //c-inherited member:TrackingCode
+            //D-maintainability and reusability:modify the inherited menbers in single unit(parent class)
+            #endregion
         }
     }
 }
