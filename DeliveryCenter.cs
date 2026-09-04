2337be663059e64837dec08d016dcc0eb7cf0ae0
Esraa Mohamed <esomo60@gmail.com>
2026-09-04 17:08:21 +0300

    Practical Questions-Q1-covert from struct  to class

---
+++ DeliveryCenter.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOOP02
+{
+    internal class DeliveryCenter
+    {
+        #region field
+        private Shipment[] shipments;
+        #endregion
+        #region constructor
+        public DeliveryCenter()
+        {
+            shipments = new Shipment[10];
+        }
+
+        #endregion
+        #region properties
+        //integer indexer
+        public Shipment this[int position]
+        {
+            get
+            {
+                if (shipments != null && position >= 0 && position < 10)
+                    return shipments[position];
+                return default;
+            }
+            set
+            {
+                if (shipments != null && position >= 0 && position < 10)
+                    shipments[position] = value;
+            }
+        }
+        //string indexer
+        public Shipment this[string trackingCode]
+        {
+            get
+            {
+                if (shipments != null)
+                {
+                    for (int i = 0; i < 10; i++)
+                    {
+                        if (shipments[i].TrackingCode == trackingCode)
+                        {
+                            return shipments[i];
+
+                        }
+
+
+                    }
+                }
+                return default;
+            }
+
+        }
+
+        #endregion
+        #region method
+        public bool AddShipment(Shipment shipment)
+        {
+            if (shipments != null)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    if (shipments[i].TrackingCode is null)
+                    {
+                        shipments[i] = shipment;
+                        return true;
+                    }
+
+                }
+
+            }
+            return false;
+
+
+        }
+        #endregion
+    }
+}
---
+++ Shipment.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AssignmentOOP02
+{
+    internal class Shipment
+    {
+        #region Fields
+        // private string trackingCode; //backing field
+        private string description;
+        private decimal weight;
+        private decimal deliveryFee;
+        #endregion
+        #region Properties
+        public string TrackingCode { get; }
+        public string Description
+        {
+            get { return description; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                    description = value;
+            }
+        }
+        public decimal Weight
+        {
+            get { return weight; }
+            set
+            {
+                if (value > 0)
+                    weight = value;
+            }
+        }
+        public decimal DeliveryFee
+        {
+            get { return deliveryFee; }
+            private set
+            {
+                if (value > 0)
+                    deliveryFee = value;
+            }
+        }
+        public DeliveryAddress Destination { get; set; }
+        public decimal EstimatedCost => DeliveryFee + (Weight * 5);
+        #endregion
+        #region Constructors
+
+        public Shipment(string trackingCode, string description, decimal weight, decimal deliveryFee, DeliveryAddress destination)
+        {
+            if (!string.IsNullOrWhiteSpace(trackingCode))
+                TrackingCode = trackingCode;
+
+            Description = description;
+            Weight = weight;
+            DeliveryFee = deliveryFee;
+            Destination = destination;
+        }
+        public Shipment(string trackingCode):this(trackingCode,"unknown",1,50,new DeliveryAddress("un known","unknown",0))
+        {
+           
+          
+        }
+       
+        #endregion
+        #region methods
+        public void UpdateDeliveryFee(decimal newFee)
+        {
+            if (newFee > 0)
+                DeliveryFee = newFee;
+        }
+        public void PrintShipment()
+        {
+            Console.WriteLine($"TrackingCode:{TrackingCode}");
+            Console.WriteLine($"Description:{Description}");
+            Console.WriteLine($"Weight:{Weight}");
+            Console.WriteLine($"DeliveryFee:{DeliveryFee}");
+            Console.WriteLine($"Destination:{Destination.GetFullAdress()}");
+            Console.WriteLine($"EstimatedCost:{EstimatedCost}");
+
+
+        }
+        #endregion
+    }
+}
